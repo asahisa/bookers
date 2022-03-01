285b89fa810ba050eab4f5f63534dc523d0b6e2f
asahisa <98754141+asahisa@users.noreply.github.com>
2022-03-01 15:56:31 +0000

    [Update] 3-12 投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get "/top" => "homes#top"
-  resources :books
   
+  get "/books/:id" => "books#show", as: "book"
+
   # get 'books/new'
   # get "/books" => "books#index"
   # get 'books/show'
   # get 'books/edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+resources :books
 end
