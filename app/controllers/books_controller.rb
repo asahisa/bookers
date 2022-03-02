583d440c68473334d8329984b2cafb74555e61a5
asahisa <98754141+asahisa@users.noreply.github.com>
2022-03-02 15:06:24 +0000

    [Add] show, editぺージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def create
     book = Book.new(book_params)
@@ -18,7 +16,15 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
 
   private
   def book_params
