e881ad0cce59167a058b73362d4ce4075c5e9480
asahisa <98754141+asahisa@users.noreply.github.com>
2022-03-03 14:26:59 +0000

    [Add] destroy機能, バリテーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,17 @@
 class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -20,11 +28,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
 
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root "homes#top"
   get "/top" => "homes#top"
-
-  get "books/:id" => "books#show", as: "book"
-  patch "books/:id" => "books#update", as: "update_book"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 resources :books
 end
